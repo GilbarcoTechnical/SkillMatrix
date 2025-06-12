b0539856e8f1816021f7b2e76e8aa547e58be418
GilbarcoTechnical <gilbarcotechnical@gmail.com>
2025-06-12 17:15:46 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB3J59thQDOv7miZPP2wZewMpW8ydEfBLU",
+  authDomain: "exam-for-fse.firebaseapp.com",
+  projectId: "exam-for-fse",
+  storageBucket: "exam-for-fse.firebasestorage.app",
+  messagingSenderId: "639657745175",
+  appId: "1:639657745175:web:d5abcade4fb49d6109b552",
+  measurementId: "G-7DNRW0J94K"
+};
